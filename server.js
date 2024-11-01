5018709707143a7d2e8e66b297ca00dd23ba52ca
Zhuoheng Li <40489953+Andy-LZH@users.noreply.github.com>
2024-11-01 17:16:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const url = require("url");
 const https = require("https");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors()); // Enable CORS for the frontend
