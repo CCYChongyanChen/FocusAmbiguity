a884daa69e23620fe29cc6007c6ad279e61b5f96
Zhuoheng Li <40489953+Andy-LZH@users.noreply.github.com>
2024-11-06 12:23:02 -0700

    Major Production Updates
    Server
    ---
    - Update Process.py to clean all data with correct json files, this version will contain selected results but not on production
    
    Client
    ---
    1. Objects polygon now also rendering based on size, and the index is also checked and correct
    2. Now objects and parts labels also show up even when question not selected
    3. Add a text showing how many segmentations are selected
    4. Minor bug fixes with rendering color

--- data/process.py
+++ data/process.py
@@ -2,26 +2,32 @@ import json
 import os
 
 # Path to your JSON file
-data_file_path = "ivc-unambigous.json"
+data_file_paths = ["ivc-unambigous.json", "ivc-ambigous.json"]
 
-# Read the JSON data
-with open(data_file_path, "r", encoding="utf-8") as file:
-    data = json.load(file)
+for data_file_path in data_file_paths:
+    # Read the JSON data
+    with open(data_file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
 
-# Iterate over each entry in the data
-for entry in data:
-    # Check if 'questions' exists and 'original_questions' is missing, and add if needed
-    if "questions" in entry and "original_questions" not in entry:
-        entry["original_questions"] = entry["questions"][:]
+    # Iterate over each entry in the data
+    for entry in data:
+        # Check if 'questions' exists and 'original_questions' is missing, and add if needed
+        if "questions" in entry and "original_questions" not in entry:
+            entry["original_questions"] = entry["questions"][:]
 
-    # Check if 'imageURL' exists and starts with 'http'
-    if "imageURL" in entry and entry["imageURL"].startswith("http://"):
-        entry["imageURL"] = entry["imageURL"].replace("http://", "https://", 1)
+        # Check if 'imageURL' exists and starts with 'http'
+        if "imageURL" in entry and entry["imageURL"].startswith("http://"):
+            entry["imageURL"] = entry["imageURL"].replace("http://", "https://", 1)
 
-# Write the updated data back to the JSON file
-with open(data_file_path, "w", encoding="utf-8") as file:
-    json.dump(data, file, indent=2, ensure_ascii=False)
+        # check all entry start with selected_ and make them empty
+        for key in entry.keys():
+            if key.startswith("selected_"):
+                entry[key] = []
 
-print(
-    "Updated JSON file: added original_questions where missing and changed all imageURLs to start with https."
-)
+    # Write the updated data back to the JSON file
+    with open(data_file_path, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+    print(
+        "Updated JSON file: added original_questions where missing and changed all imageURLs to start with https."
+    )
